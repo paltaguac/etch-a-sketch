69a4d14852b53f2f5e09d2d87a5fe00380cd22b3
Martini <paltaguac@gmail.com>
2022-04-25 19:08:09 -0400

    Made the divs background turn black when hovered on

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const wrapper = document.querySelector(".wrapper")
-const gridContainer = document.querySelector(".gridContainer")
-function makeRows(rows, cols){
-    gridContainer.style.setProperty("--grid-rows", rows);
-    gridContainer.style.setProperty("--grid-cols",cols);
-    for (i=0; i<(rows*cols);i++){
-        let cell = document.createElement("div");
-        gridContainer.appendChild(cell).className="grid-item";
-    }
+const wrapper = document.querySelector(".wrapper");
+const gridContainer = document.querySelector(".gridContainer");
+function makeRows(rows, cols) { // this function will create our grid dynamically.
+  gridContainer.style.setProperty("--grid-rows", rows);
+  gridContainer.style.setProperty("--grid-cols", cols);
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    gridContainer.appendChild(cell).className = "gridItem";
+  }
 }
 
-makeRows(16,16)
+makeRows(16, 16); // here we are calling the function to create our grid with 16 rows and 16 columns
+const gridItem = document.querySelector(".gridItem")
+const squares = document.querySelectorAll(".gridItem")
+squares.forEach((gridItem) => { // Here we are adding the change of color to every div(square) of our grid(gridContainer)
+    gridItem.addEventListener("mouseenter",function(event){
+        event.target.style.backgroundColor = "black";
+    })
+});
