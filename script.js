28be2b4ca8fa4577e99eb16aa7e388d22b3e1eb8
Martini <paltaguac@gmail.com>
2022-04-25 20:07:16 -0400

    added comments

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ function makeRows(rows, cols) { // this function will create our grid dynamicall
 }
 
 makeRows(16, 16); // here we are calling the function to create our grid with 16 rows and 16 columns
-const gridItem = document.querySelector(".gridItem")
-const squares = document.querySelectorAll(".gridItem")
-function changeColor(color){
+
+const gridItem = document.querySelector(".gridItem")// each grid item is a square
+const squares = document.querySelectorAll(".gridItem")// selecting all of the squares inside the grid
+function changeColor(color){// we are creating a function to change color on hover.
     squares.forEach(gridItem => {
         gridItem.addEventListener("mouseenter",function(event){
             event.target.style.backgroundColor = color;
     });
 })};
-const clear = document.querySelector("#clear")
+
+const clear = document.querySelector("#clear")//Here we are selecting the clear button
 clear.addEventListener("click",function(){
     squares.forEach(gridItem=>{gridItem.setAttribute("style","backgroundColor:none")})
-})
+})//here we are adding the function to the clear button, so that on click it will go thhrough each square in the grid and change its background color.
 changeColor("black")
