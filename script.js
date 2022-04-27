1a3649456f1a375ff865183dfb82cd519ade7f04
Martini <paltaguac@gmail.com>
2022-04-27 10:53:52 -0400

    changed make rows from cols and rows, to just size as i want the grid to stay even in rows and cols

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const wrapper = document.querySelector(".wrapper");
 const grid = document.querySelector(".grid");
-function makeRows(rows, cols) { // this function will create our grid dynamically.
-  grid.style.setProperty("--grid-rows", rows);
-  grid.style.setProperty("--grid-cols", cols);
-  for (i = 0; i < rows * cols; i++) {
+function makeRows(size) { // this function will create our grid dynamically.
+  grid.style.setProperty("--grid-rows", size);
+  grid.style.setProperty("--grid-cols", size);
+  for (i = 0; i < size**2; i++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "gridItem";
   }
 }
 
-makeRows(32, 32); // here we are calling the function to create our grid with 16 rows and 16 columns
+makeRows(16); // here we are calling the function to create our grid with 16 rows and 16 columns
 
 const gridItem = document.querySelector(".gridItem")// each grid item is a square
 const squares = document.querySelectorAll(".gridItem")// selecting all of the squares inside the grid
